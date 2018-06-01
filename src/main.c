abdb1ced0e53214c30115c56eda185f1fde0aee7
Allen Temunovic <ajtemunovic@gmail.com>
2018-05-31 22:53:18 -0700

    added .gitignore

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: atemunov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/28 23:11:32 by atemunov          #+#    #+#             */
-/*   Updated: 2018/05/31 21:52:03 by atemunov         ###   ########.fr       */
+/*   Updated: 2018/05/31 22:43:26 by atemunov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,9 @@ int	main(void)
 	ft_printf("%%\n");
 	ft_printf("%c\n", 'a');
 	ft_printf("%C\n", 'a');
+	ft_printf("%s\n", "42Kathisd asdfasdg");
 	// printf("%c\n", 'a');
 	// printf("%C\n", 'a');
-	// printf("%o\n", 1000);
-	// printf("%O\n", 1000);
 	ft_printf("%u\n", 42);
 	ft_printf("%U\n", 42);
 	ft_printf("%x\n", 11);
