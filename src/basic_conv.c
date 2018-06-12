a6b55bee4af1365a69227c60d2493fc0f1e633bc
Allen Temunovic <ajtemunovic@gmail.com>
2018-06-12 16:51:14 -0700

    test

--- src/basic_conv.c
+++ src/basic_conv.c
@@ -36,7 +36,7 @@ char	*print_memory_address(unsigned long int nbr, t_flags *pass)
 	length = 0;
 	print = malloc(sizeof(char) * length + 1);
 	if (nbr == 0)
-		print[i] = '0';
+		print = "0";
 	while (nbr != 0)
 	{
 		print[i++] = H_LOWER[nbr % 16];
@@ -45,7 +45,6 @@ char	*print_memory_address(unsigned long int nbr, t_flags *pass)
 	print[i] = '\0';
 	print = str_rev(print);
 	print = ft_strjoin("0x", print);
-	print = ft_strjoin(print, "0");
 	return (print);
 }
 
