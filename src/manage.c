ef1cb2c3a8cf88b02353bdbf55feacb59d6d0e97
Allen Temunovic <ajtemunovic@gmail.com>
2018-06-13 00:11:26 -0700

    now it should work

--- src/manage.c
+++ src/manage.c
@@ -90,7 +90,7 @@ void	ft_apply_flagmods(char *format, t_flags *flags)
 	format = print_spaces(format, flags);
 	format = (flags->plus && flags->conversion == 'x' && flags->zero) ? ft_strjoin("0x", format) : format;
 	format = (flags->plus && flags->conversion == 'X' && flags->zero) ? ft_strjoin("0X", format) : format;
-	format = (flags->space && format[0] != '-') ? ft_strjoin("", format) : format;
+	format = (flags->space && format[0] != '-') ? ft_strjoin(" ", format) : format;
 	ft_putstrf(format, flags, 0);
 } 	
 
