bdee73f6bf8cef4ab4f9e45b2602603044f6c9c4
Allen Temunovic <ajtemunovic@gmail.com>
2018-06-13 22:36:22 -0700

    minor adjustment

--- src/main.c
+++ src/main.c
@@ -48,6 +48,8 @@ int	main(void)
 //	ft_printf("%o\n", -0);
 //	ft_printf("%o\n", 0);
 // 	ft_printf("normal no k = [%S]\n", L"갔갔");
-	ft_printf("%#o\n", INT_MAX);
+//	ft_printf("%#o\n", INT_MAX);
+	ft_printf("%s\n", "pouet pouet camembert !!");
+//	printf("%s\n", "pouet pouet camembert !!");
 	return (0);
 }
