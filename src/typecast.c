47b1b3fed39754eb2f51b03bae8741928d3282d2
Allen Temunovic <ajtemunovic@gmail.com>
2018-06-14 19:43:46 -0700

    test

--- src/typecast.c
+++ src/typecast.c
@@ -6,7 +6,7 @@
 /*   By: atemunov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/11 14:12:09 by atemunov          #+#    #+#             */
-/*   Updated: 2018/06/14 19:41:51 by atemunov         ###   ########.fr       */
+/*   Updated: 2018/06/14 19:43:26 by atemunov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*typecast_mod(va_list list, t_flags *flags, int base)
 	else if (flags->modifier == 3)
 			return (ft_itoa_smax(va_arg(list, long)));
 	else if (flags->modifier == 4)
-			return (ft_itoa_umax(va_arg(list, long long)));
+			return (ft_itoa_smax(va_arg(list, long long)));
 	else if (flags->modifier == 2)
 			return (ft_itoaf((short)va_arg(list, int), flags, 1, 0));
 	else if (flags->modifier == 1)
